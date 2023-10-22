57960db67c40a0722da64ac94d837dcef56d9654
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-10-22 17:58:32 -0400

    [Add] CRUD for Person

---
+++ app/Http/Requests/Person/StorePersonRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Person;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePersonRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'team_id' => 'required|exists:teams,id',
+            'role_id' => 'required|exists:roles,id',
+            'name' => 'required|string',
+            'lastname' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'birthday' => 'required|date',
+            'phone' => 'required|string',
+        ];
+    }
+}
