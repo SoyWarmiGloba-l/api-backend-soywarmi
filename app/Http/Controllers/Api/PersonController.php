2ce261874ffa6bba8dbff92d9d9553dfbe2e2ed9
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-10-22 17:58:32 -0400

    [Add] CRUD for Person

--- app/Http/Controllers/Api/PersonController.php
+++ app/Http/Controllers/Api/PersonController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Person;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class PersonController extends Controller
 {
@@ -12,15 +15,11 @@ class PersonController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        try {
+            return responseJSON(Person::with('doctor', 'team')->get(), 200, 'Success');
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 500);
+        }
     }
 
     /**
@@ -28,38 +27,68 @@ class PersonController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $url = Storage::disk('public')->put($request->image->getClientOriginalName(), $request->image);
+            $request->merge([
+                'photo' => env('APP_URL').'/storage/'.$url,
+                'password' => Hash::make($request->password),
+            ]);
+            $person = Person::create($request->except('image'));
+
+            return responseJSON($person, 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, 500, $e->getMessage());
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Person $person)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        try {
+            return responseJSON($person->load('doctor', 'team'), 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, $e->getMessage(), 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Person $person)
     {
-        //
+        try {
+            if (isset($request->image)) {
+                $url = Storage::disk('public')->put($request->image->getClientOriginalName(), $request->image);
+                $request->merge([
+                    'photo' => env('APP_URL').'/storage/'.$url,
+                ]);
+            }
+            if (isset($request->password)) {
+                $request->merge([
+                    'password' => Hash::make($request->password),
+                ]);
+            }
+            $person->update($request->all());
+
+            return responseJSON($person, 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, $e->getMessage(), 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Person $person)
     {
-        //
+        try {
+            $person->delete();
+
+            return responseJSON($person, 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, $e->getMessage(), 500);
+        }
     }
 }
