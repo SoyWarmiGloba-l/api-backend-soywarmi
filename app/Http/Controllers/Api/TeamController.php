7dc3ec1c60b6e7f7902599dd8c71569880a59ea7
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-10-22 16:21:09 -0400

    [Add] CRUD for Teams

---
+++ app/Http/Controllers/Api/TeamController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Team;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Team\StoreTeamRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class TeamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            return responseJSON(Team::with('person', 'role')->get(), 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, 500, $e->getMessage());
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTeamRequest $request)
+    {
+        try {
+            $team = Team::create($request->validated());
+            return responseJSON($team, 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, 500, $e->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Team $team)
+    {
+        try {
+            return responseJSON($team->load('person', 'role'), 200, 'Success');
+        } catch (ModelNotFoundException $e) {
+            return responseJSON(null, 404, $e->getMessage());
+        } catch (\Exception $e) {
+            return responseJSON(null, 500, $e->getMessage());
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Team $team)
+    {
+        try {
+            $team->update($request->all());
+            return responseJSON($team, 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, 500, $e->getMessage());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Team $team)
+    {
+        try {
+            $team->delete();
+            return responseJSON($team, 200, 'Success');
+        } catch (\Exception $e) {
+            return responseJSON(null, 500, $e->getMessage());
+        }
+    }
+}
