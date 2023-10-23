4124efd9aa1c4aba72364a2dee8d2b96d19ef6d2
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-10-23 14:15:17 -0400

    [Add] Authentication and auth api resources

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             TeamSeeder::class,
             PersonSeeder::class,
             DoctorSeeder::class,
+            UserSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
