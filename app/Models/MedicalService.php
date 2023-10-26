30784ab51048fd2c23311bfae98ad64a35d69f09
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-10-26 15:59:47 -0400

    [Add] model,migration, seeder and factory from the tables medical_service and medical_center

--- app/Models/MedicalService.php
+++ app/Models/MedicalService.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\MedicalService;
+use App\Models\MedicalCenter;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class MedicalCenter extends Model
+class MedicalService extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
 
-    public function medicalServices(): BelongsToMany
+    public function medicalCenters(): BelongsToMany
     {
-        return $this->belongsToMany(MedicalService::class);
+        return $this->belongsToMany(MedicalCenter::class);
     }
 }
