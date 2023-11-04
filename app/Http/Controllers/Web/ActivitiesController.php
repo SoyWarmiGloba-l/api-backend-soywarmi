9e51b640effbf67619faaf9d0120259eff5c94bd
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-11-04 13:10:07 -0400

    [Add] admin function activities and update views news

--- app/Http/Controllers/Web/ActivitiesController.php
+++ app/Http/Controllers/Web/ActivitiesController.php
@@ -13,33 +13,60 @@ class ActivitiesController extends Controller
     public function index()
     {
         $eventTypes = EventType::all();
-        return view('admin.activities_index', compact('eventTypes'));
+        $activities = Activity::select('id', 'event_type_id', 'name', 'description', 'end_date')->with('eventType')->get();
+        return view('admin.activities_index', compact('eventTypes', 'activities'));
+    }
+
+    public function getActivities(Request $request)
+    {
+        $activities = Activity::where('id', $request->id)->with('eventType', 'images')->get();
+        return responseJSON($activities, 200, 'Actividad encontrada');
+    }
+
+    public function deleteactivity(Activity $activity)
+    {
+        $activity->delete();
+        session()->flash('success', 'Actividad eliminada');
+        return redirect()->back();
     }
     public function saveActivity(Request $request)
     {
-        $activity = Activity::create([
+        if($request->save == "true"){
+            $activity = Activity::create([
+                'event_type_id' => $request->eventType,
+                'name' => $request->name,
+                'description' => $request->description,
+                'end_date' => $request->dataEnd,
+                'step' => json_encode($request->steps),
+                'area' => json_encode($request->areas),
+                'requirement' => json_encode($request->requirements),
+            ]);
+            if ($request->file('kifPholder'))
+            {
+                $files = $request->file('kifPholder');
+                if (count($files) > 0) {
+                    foreach ($files as $file) {
+                        $image = new Image();
+                        $image->name = $file->getClientOriginalName();
+                        $image->type = '.'. $file->getClientOriginalExtension();
+                        $image->url = '/storage/' . Storage::disk('public')->putFile('images', $file);
+                        $activity->images()->save($image);
+                    }
+                }
+            }
+            session()->flash('success', 'Actividad creada');
+            return redirect()->back();
+        }
+        Activity::where('id', $request->id)->update([
             'event_type_id' => $request->eventType,
             'name' => $request->name,
             'description' => $request->description,
             'end_date' => $request->dataEnd,
-            'step' => json_encode($request->steps),
-            'area' => json_encode($request->areas),
-            'requirement' => json_encode($request->requirements),
+            'step' => isset($request->steps) ? json_encode($request->steps) : null,
+            'area' => isset($request->areas) ? json_encode($request->areas) : null,
+            'requirement' => isset($request->requirements) ? json_encode($request->requirements) : null,
         ]);
-        if ($request->file('kifPholder'))
-        {
-            $files = $request->file('kifPholder');
-            if (count($files) > 0) {
-                foreach ($files as $file) {
-                    $image = new Image();
-                    $image->name = $file->getClientOriginalName();
-                    $image->type = '.'. $file->getClientOriginalExtension();
-                    $image->url = '/storage/' . Storage::disk('public')->putFile('images', $file);
-                    $activity->images()->save($image);
-                }
-            }
-        }
-        session()->flash('success', 'Actividad creada');
+        session()->flash('success', 'Actividad actualizada');
         return redirect()->back();
     }
 }
