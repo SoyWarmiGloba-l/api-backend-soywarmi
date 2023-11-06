930eedbbb02084a30c3efcae3ecd7dde30af4945
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-11-06 17:16:08 -0400

    fixed conflict login with firebase

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
+            'id' => '1',
             'role' => 'Admin',
             'name' => 'Admin 1',
             'email' => 'email1@gmail.com',
