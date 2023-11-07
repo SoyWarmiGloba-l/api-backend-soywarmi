d871b464cc68ae45565061e45d8572d9904b8bb6
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-11-07 13:51:29 -0400

    [Add] update from helper auth api login firebase

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -18,6 +18,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
+
         $token = new FirebaseToken($request->bearerToken());
 
         try {
