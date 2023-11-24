1f11791997fb893f9b95639edd668692c2bdaf39
Angel Nayib Espinoza Ibañez <50686805+Angel9817@users.noreply.github.com>
2023-11-24 19:08:49 -0400

    [Add] se agraga la administración de los equipos

--- app/Http/Controllers/Web/TeamsController.php
+++ app/Http/Controllers/Web/TeamsController.php
@@ -18,6 +18,40 @@ class TeamsController extends Controller
         $roles = Role::all();
         //dd($roles);
 
-        return view('admin.team_index', compact('roles'));
+        return view('admin.team_index', compact('roles', 'teams'));
+    }
+
+    public function getTeam(Request $request)
+    {
+        $team = Team::where('id', $request->id)->with('role', 'person')->get();
+        return responseJSON($team, 200, 'Equipo encontrada');
+    }
+
+    public function storeTeam(Request $request)
+    {
+
+        try {
+            if ($request->save == "true") {
+                Team::create([
+                    'role_id' => $request->role,
+                    'name' => $request->name,
+                    'description' => strip_tags($request->description),
+                    'social_networks' => json_encode($request->social),
+                ]);
+                session()->flash('success', 'Equipo creado');
+                return redirect()->back();
+            }
+            Team::where('id', $request->id)->update([
+                'role_id' => $request->role,
+                'name' => $request->name,
+                'description' => strip_tags($request->description),
+                'social_networks' => isset($request->social) ? json_encode($request->social) : null,
+            ]);
+            session()->flash('success', 'Equipo actualizada');
+            return redirect()->back();
+        } catch (\Exception $e) {
+            session()->flash('success', 'Persona actualizada');
+            return redirect()->back();
+        }
     }
 }
