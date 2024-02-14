a0eb3fb78c1601fa650b95f7522c20e603f48df4
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-02-14 10:50:25 -0400

    [Fix] chat participation

--- app/Models/ChatParticipations.php
+++ app/Models/ChatParticipations.php
@@ -4,12 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class ChatParticipations extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    protected $primaryKey = "id_chat_participation";
 
     public function chatConversations(): BelongsTo
     {
