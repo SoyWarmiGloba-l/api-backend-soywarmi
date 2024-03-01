79d2bb0697bc83891f0f9cd772adcb2d41ee53e6
VictorVillazante <villazante.bernabe.victor@gmail.com>
2024-03-01 09:19:49 -0400

    [Fix] arreglando merge con repositorio remoto

--- routes/api.php
+++ routes/api.php
@@ -43,45 +43,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route group with middleware and prefinx
-// Route::middleware('auth:api')->prefix('v1')->group(function () {
-//     // Route::resource('teams', TeamController::class);
-//     // Route::resource('people', PersonController::class);
-//     // Route::resource('medical_centers', MedicalCenterController::class);
-//     // Route::resource('medical_services', MedicalServiceController::class);
-//     // Route::resource('publications', PublicationController::class);
-//     // Route::resource('comments', CommentController::class);
-//     // Route::resource('news', NewsController::class);
-//     // Route::resource('event_types', EventTypeController::class);
-//     // Route::resource('activities', ActivityController::class);
-//     // Route::resource('resources', ResourceController::class);
-//     // Route::resource('faqs', FaqController::class);
-//     // Route::resource('testimonies', TestimonyController::class);
-//     // Route::get('testimonies/slug/{slug}', [TestimonyController::class, 'showSlug']);
-//     // Route::resource('images', ImageController::class)->except('store', 'update');
-//     Route::resource('doctors', DoctorController::class);
-//     Route::resource('news', NewsController::class);
-//     Route::resource('teams', TeamController::class);
-//     Route::resource('people', PersonController::class);
-//     Route::resource('medical_centers', MedicalCenterController::class);
-//     Route::resource('medical_services', MedicalServiceController::class);
-//     Route::resource('publications', PublicationController::class);
-//     Route::resource('comments', CommentController::class);
-//     Route::resource('event_types', EventTypeController::class);
-//     Route::resource('activities', ActivityController::class);
-//     Route::resource('resources', ResourceController::class);
-//     Route::resource('faqs', FaqController::class);
-//     Route::resource('testimonies', TestimonyController::class);
-//     Route::get('testimonies/slug/{slug}', [TestimonyController::class, 'showSlug']);
-//     Route::resource('images', ImageController::class)->except('store', 'update');
-//     Route::get('get_messages/{chat}', [ChatMessagesParticipation::class, 'obtenerChatMessagesConversation']);
-//     Route::post('post_message/{chat}', [ChatMessagesParticipation::class, 'postMessage']);
-    
-// });
-Route::group([
-    'prefix' => 'v1',
-], function ($router) {
-
+//Route group with middleware and prefinx
+Route::middleware('auth:api')->prefix('v1')->group(function () {
     Route::resource('doctors', DoctorController::class);
     Route::resource('news', NewsController::class);
     Route::resource('teams', TeamController::class);
@@ -95,8 +58,11 @@ Route::group([
     Route::resource('resources', ResourceController::class);
     Route::resource('faqs', FaqController::class);
     Route::resource('testimonies', TestimonyController::class);
-    Route::get('testimonies/slug/{slug}', [TestimonyController::class, 'showSlug']);
     Route::resource('images', ImageController::class)->except('store', 'update');
+
+    Route::get('testimonies/slug/{slug}', [TestimonyController::class, 'showSlug']);
+    Route::get('get_messages/{chat}', [ChatMessagesParticipation::class, 'obtenerChatMessagesConversation']);
+    Route::post('post_message/{chat}', [ChatMessagesParticipation::class, 'postMessage']);
 
     Route::delete('/chat_conversations/{chatConversation}', [ChatConversationsController::class, 'destroy']);
     Route::delete('/chat_messages_participation/{chatMensagesParticipation}', [ChatMessagesParticipation::class, 'destroy']);
@@ -104,8 +70,7 @@ Route::group([
     Route::post('/subscribe', [SubscribeController::class, 'store']);
     Route::post('/subscribe/{hash}', [SubscribeController::class, 'show'])->name('subscribe.show');
     Route::get('chat_conversations',[ChatConversationsController::class,'obtenerConversacionesUsuario']);
-    Route::get('get_messages/{chat}', [ChatMessagesParticipation::class, 'obtenerChatMessagesConversation']);
-    Route::post('post_message/{chat}', [ChatMessagesParticipation::class, 'postMessage']);
+   
     Route::get('get_users', [PersonController::class, 'getPeopleToChat']);
     Route::post('register_chat_conversation', [ChatConversationsController::class, 'registerChatConversation']);
     Route::put('check_read_message/{conversationId}', [ChatMessagesParticipation::class, 'checkReadMessage']);
@@ -120,18 +85,17 @@ Route::group([
     Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
     Route::post('/password/reset', [ResetPasswordController::class, 'reset']);
     Route::get('/recent_publications', [PublicationController::class, 'getAllOrderDescByCreatedAt']);
-
+    Route::get('/password_recovery', [AuthController::class, 'paswordRecoveryFirebase']);
+    Route::get('awards/all', [AwardController::class, 'index']);
+    
 });
-Route::get('/verify_connection', function () {
-    return response()->json(['message' => 'Conexión exitosa']);
-});
-
-// Route::post('post_1', [MensajesAuxController::class, 'save']);
-// Route::get('get_1', [MensajesAuxController::class, 'get_1']);
 
 Route::post('auth/register', [AuthController::class, 'createGeneralPublicUser']);
 Route::post('auth/login', [AuthController::class, 'login']);
 
+Route::get('/verify_connection', function () {
+    return response()->json(['message' => 'Conexión exitosa']);
+});
 
 Route::group([
 
@@ -144,12 +108,12 @@ Route::group([
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
 });
-// Route::middleware('auth:api')->prefix('v1')->group(
-//     function () {
-//         Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
-//         Route::post('/password/reset', [ResetPasswordController::class, 'reset']);
-//     }
-// );
+Route::middleware('auth:api')->prefix('v1')->group(
+    function () {
+        Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail']);
+        Route::post('/password/reset', [ResetPasswordController::class, 'reset']);
+    }
+);
 
 
 Route::get('routes', function () {
